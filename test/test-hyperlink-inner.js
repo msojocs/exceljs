65728fb069afb89303af7324d767f6fd69e586ce
msojocs <jiyecafe@gmail.com>
2022-08-16 21:47:57 +0800

    fix: hyperlink of adding

--- test/test-hyperlink-inner.js
+++ test/test-hyperlink-inner.js
@@ -5,11 +5,13 @@ const Excel = require('../lib/exceljs.nodejs.js');
 const HrStopwatch = require('./utils/hr-stopwatch');
 
 const filename = path.resolve(__dirname, 'data/hyperlink.xlsx');
-const filename_out = path.resolve(__dirname, 'data/hyperlink-out.xlsx');
+const filenameOut = path.resolve(__dirname, 'data/hyperlink-out.xlsx');
 
 const test = async () =>{
 
   const wb = new Excel.Workbook();
+  
+  await wb.xlsx.readFile(filename);
   const ws = wb.addWorksheet('Foo');
   
   ws.getCell('A1').value = {
@@ -17,8 +19,11 @@ const test = async () =>{
     text: 'ExcelJS',
     tooltip: 'https://www.npmjs.com/package/exceljs',
   };
-  
-  await wb.xlsx.readFile(filename);
+  ws.getCell('B1').value = {
+    location: 'Sheet1!A1',
+    text: 'Sheet1',
+    tooltip: 'Go To Sheet1',
+  };
   const ws1 = wb.getWorksheet('Sheet1');
   console.log('================over===========');
   console.log(ws1.getCell(1,1).isHyperlink, ws1.getCell(1,1).hyperlink);
@@ -26,7 +31,7 @@ const test = async () =>{
   console.log(ws1.getCell(1,2).isHyperlink, ws1.getCell(1,2).value);
   console.log(ws1.getCell(1,2).model);
   console.log(ws1.hyperlinks);
-  wb.xlsx.writeFile(filename_out);
+  wb.xlsx.writeFile(filenameOut);
  
   
 };
