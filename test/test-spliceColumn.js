7763c8322df45fd7d8a87e5ca6a95a09d8f338b3
msojocs <jiyecafe@gmail.com>
2022-09-10 18:54:24 +0800

    fix: splice with merge

--- test/test-spliceColumn.js
+++ test/test-spliceColumn.js
@@ -2,8 +2,8 @@
 const path = require('path');
 const ExcelJS = require('../lib/exceljs.nodejs');
 
-const TEST_XLSX_FILE_NAME = path.resolve(__dirname, '../data/splice.xlsx');
-const filenameOut = path.resolve(__dirname, '../data/splice-out.xlsx');
+const TEST_XLSX_FILE_NAME = path.resolve(__dirname, './data/splice-example.xlsx');
+const filenameOut = path.resolve(__dirname, './data/splice-out.xlsx');
 const options = {
     filename: TEST_XLSX_FILE_NAME,
     useStyles: true,
@@ -13,11 +13,11 @@ const wb = new ExcelJS.Workbook(options);
 wb.xlsx.readFile(TEST_XLSX_FILE_NAME).then(async () => {
 
     const ws = wb.getWorksheet('Sheet1');
-    // ws.spliceColumns(4, 2);
-    ws.spliceColumns(7, 1);
-    ws.spliceColumns(6, 1);
+    ws.spliceColumns(4, 1);
+    ws.spliceColumns(4, 1);
+    // ws.spliceColumns(5, 1);
 
-    ws.spliceColumns(6, 0, [undefined, 1, 2, 3]);
+    // ws.spliceColumns(6, 0, [undefined, 1, 2, 3]);
 
     await wb.xlsx.writeFile(filenameOut);
 });
